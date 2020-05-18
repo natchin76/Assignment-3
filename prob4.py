b03b4b774db3aadfda980ed44181ae9b16ebb9a5
Chintan Patel <chintanpatel220598@gmail.com>
2020-05-18 20:43:27 +0530

    Update prob4.py

--- prob4.py
+++ prob4.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May 13 23:50:47 2020
-Prob4: Fourier transform of gaussian
-@author: chintan
-"""
 import numpy as np
 from matplotlib import pyplot as plt
 fname='prob4.txt'
@@ -12,7 +5,8 @@ ft_c=np.loadtxt(fname,usecols=0)
 k_c=np.loadtxt(fname,usecols=1)
 def f(k):
     return(1/np.sqrt(2)*np.exp(-k**2/4))
-plt.scatter(k_c,ft_c)
+plt.scatter(k_c,ft_c,label='using FFTW')
 k=np.linspace(-8,8,50)
-plt.plot(k,f(k))
+plt.plot(k,f(k),label='Analytical')
+plt.legend()
 plt.show()
