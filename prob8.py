cbbfd78b573ddbd6f365dee1169044646513f10d
Chintan Patel <chintanpatel220598@gmail.com>
2020-05-18 23:11:23 +0530

    Update prob8.py

--- prob8.py
+++ prob8.py
@@ -12,6 +12,9 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 def f(x,y):
     return(np.exp(-(x**2+y**2)))
+def fk_ext(kx,ky):
+     kx,ky=np.meshgrid(kx,ky)
+     return np.exp(-(kx**2+ky**2)/4)/2   
 xmin=-25
 xmax=25
 ymin=-25
@@ -32,10 +35,18 @@ fctry=np.exp(-1j*kyarr*ymin)
 aft=(dx*dy*(n/(2.0*np.pi))*fctrx*fctry*f_q)
 Kx, Ky=np.meshgrid(kxarr,kyarr)
 
-fig = plt.figure()
+fig = plt.figure(figsize=plt.figaspect(0.5))
 ax = fig.gca(projection='3d')
 ax.set_xlabel("kx")
 ax.set_ylabel("ky")
 surf = ax.contour3D(Kx, Ky, abs(aft),100)
+plt.title('Using FFT2')
 plt.show()
 
+fig = plt.figure(figsize=plt.figaspect(0.5))
+ax = fig.gca(projection='3d')
+ax.set_xlabel("kx")
+ax.set_ylabel("ky")
+surf2 = ax.contour3D(Kx, Ky, fk_ext(kxarr,kyarr),100)
+plt.title('Analytical')
+plt.show()
