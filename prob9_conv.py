e5bbccfe1030fd681fc47aa27101d6c2c7d11130
Chintan Patel <chintanpatel220598@gmail.com>
2020-05-18 21:33:54 +0530

    Update prob9_conv.py

--- prob9_conv.py
+++ prob9_conv.py
@@ -13,26 +13,17 @@ def f(x):
     return(1)           
 xmin=-5
 xmax=5
-n=512
+n=256
 x=np.linspace(xmin,xmax,n)
 dx=(xmax-xmin)/(n-1)
 y=np.zeros(n)
 for i in range(n):
     y[i]=f(x[i])
-karr=2*np.pi*np.fft.fftfreq(n,d=dx)
-karr=np.fft.fftshift(karr)
-fctr=np.exp(-1j*karr*xmin)
 ft=np.fft.fft(y,norm='ortho')
-aft=dx*np.sqrt(n/(2*np.pi))*fctr*ft
-gft=np.zeros(n)
-for i in range(n):
-    gft[i]=np.sqrt(2*np.pi)*aft[i]**2
-dk=karr[1]-karr[0]
-xarr=2*np.pi*np.fft.fftfreq(n,d=dk)
-ift=np.fft.ifft(gft,norm='ortho')
-fctrx=np.exp(-1j*xarr*xmin)
-con=dk*np.sqrt(n/(2*np.pi))*fctrx*ift
-plt.scatter(xarr,abs(con),label='Convolution')
+gt=ft**2
+con=np.sqrt(n)*dx*np.fft.ifft(gt,norm='ortho')
+con=np.fft.fftshift(con)
+plt.scatter(x,abs(con),label='Convolution')
 x=np.linspace(-2,2,50)
 y=np.zeros(50)
 for i in range(50):
